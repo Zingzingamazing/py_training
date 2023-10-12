4b3c5e5f667fb9a9c67bdda32acda246d4bbd06e
Zingzingamazing <zing_ing@hotmail.com>
2023-10-12 21:35:40 +1100

    If statements

---
+++ training.py
@@ -0,0 +1,15 @@
+## print('Hello world')
+
+
+## If Statements
+## If buyers have a good credit, they has a better down payment.
+price = 1
+is_good_credit = True
+
+
+if is_good_credit:
+    down_payment = price * 0.1
+else:
+    down_payment = price *0.2
+
+print(f"Down payment: ${down_payment}")
