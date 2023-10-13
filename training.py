fae7dcc1631ff95994b815081e9554b66f8270d6
Zingzingamazing <zing_ing@hotmail.com>
2023-10-13 21:17:33 +1100

    updated

--- training.py
+++ training.py
@@ -1,4 +1,4 @@
-## print('Hello world')
+print('Hello world')
 
 
 ## If Statements
@@ -13,3 +13,29 @@ else:
     down_payment = price *0.2
 
 print(f"Down payment: ${down_payment}")
+
+
+## Logical operators
+## Eligible for loan, if you have high income AND good credit
+
+has_high_income = False
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+    eligible_loan = True
+else:
+    eligible_loan = False
+
+print(eligible_loan)
+
+
+## name must be at least 3 characters
+
+name = "aasdfasdfasdfbgh"
+
+if len(name) <= 3:
+    print("name must be at least 3 characters.")
+elif len(name) > 10:
+    print("no more than 10")
+else:
+    print("it is OK")
